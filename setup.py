07ef5ee3875460a76f7e92ab8e20577f48e0c144
Dougal Sutherland <dougal@gmail.com>
2013-06-26 17:48:05 -0400

    switch to 0.1.3dev version name

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ ext_modules = [
 
 setup(
     name='cyflann',
-    version='0.1.2',
+    version='0.1.3dev',
     author='Dougal J. Sutherland',
     author_email='dougal@gmail.com',
     packages=['cyflann'],
