16ab1a79791c03fab1b223e97031722c11c7b550
Dougal Sutherland <dougal@gmail.com>
2013-10-02 02:51:19 -0400

    0.1.9-dev
    guess I skipped 0.1.7 (whoops)

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ for ext in ext_modules:
 
 setup(
     name='cyflann',
-    version='0.1.8',
+    version='0.1.9-dev',
     author='Dougal J. Sutherland',
     author_email='dougal@gmail.com',
     packages=['cyflann'],
