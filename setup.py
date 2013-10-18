8652962e1c3a4253ecf352aa73528420e136de0c
Dougal Sutherland <dougal@gmail.com>
2013-10-18 00:31:08 -0400

    release 0.1.10

--- setup.py
+++ setup.py
@@ -116,7 +116,7 @@ for ext in ext_modules:
 
 setup(
     name='cyflann',
-    version='0.1.10-dev',
+    version='0.1.10',
     author='Dougal J. Sutherland',
     author_email='dougal@gmail.com',
     packages=['cyflann', 'cyflann.tests'],
