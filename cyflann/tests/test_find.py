f61818c43ca1ed2c88a366d1cbfe2c851554c8e3
Dougal Sutherland <dougal@gmail.com>
2014-06-20 20:04:52 -0400

    fix test_find

--- cyflann/tests/test_find.py
+++ cyflann/tests/test_find.py
@@ -1,13 +1,18 @@
 import os
+import sys
 
 import cyflann
 
 def test_find_sanity():
     lib = cyflann.get_flann_lib()
     assert lib.startswith('/')
-    assert lib.endswith('.so') or lib.endswith('.dylib') or lib.endswith('.dll')
+    if sys.platform.startswith('linux'):
+        import re
+        assert re.search(r'\.so(\.\d+)*$', lib)
+    elif sys.platform == 'darwin':
+        assert lib.endswith('.dylib')
     assert os.path.isfile(lib)
 
     inc = cyflann.get_flann_include()
     assert inc.startswith('/')
-    assert os.path.isdir(lib)
+    assert os.path.isdir(inc)
