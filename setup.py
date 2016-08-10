f23355f2be191d7e473ed09c33d4e833b47ad3ce
Jason Xu <jason.xu@blippar.com>
2016-08-10 15:24:03 -0700

    Fixed error on importing: flann lib was not linked.

--- setup.py
+++ setup.py
@@ -103,6 +103,7 @@ if os.environ.get('FLANN_DIR', False):
     }
 else:
     flann_info = get_pkg_info('flann')
+    flann_info['libraries'] += ['flann']
 
 flann_info['include_dirs'] += [numpy.get_include()]
 
