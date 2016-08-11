bd6a8e24028c356a5f205427499df3de801bd5f4
Dougal J. Sutherland <dougal@gmail.com>
2016-08-11 05:01:02 -0400

    fix conda recipe

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 $PYTHON setup.py egg_info
-sed -n -e 's/^Version: \(.*\)$/\1/p' < mmd.egg-info/PKG-INFO > __conda_version__.txt
+sed -n -e 's/^Version: \(.*\)$/\1/p' < cyflann.egg-info/PKG-INFO > __conda_version__.txt
 
-FLANN_DIR=$PREFIX $PYTHON setup.py install
+$PYTHON setup.py install
