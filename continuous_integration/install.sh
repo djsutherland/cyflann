4667c57d3ca0b3177770088248dba4599f7e2a9a
Dougal J. Sutherland <dougal@gmail.com>
2016-08-10 20:01:41 -0400

    travis setup

---
+++ continuous_integration/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# the Travis "install" step: see http://docs.travis-ci.com/ and ../.travis.yml
+set -e
+
+os=$(uname)
+if [[ "$os" == "Linux" ]]; then
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh \
+        -O miniconda.sh
+elif [[ "$os" == "Darwin" ]]; then
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh \
+        -O miniconda.sh
+else
+    echo "unknown os '$os'"
+    exit 1
+fi
+chmod +x miniconda.sh
+./miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+
+conda update --yes --quiet conda
+
+conda install --yes -c dougal \
+    python=$PYTHON_VERSION pip nose setuptools cython \
+    numpy=$NUMPY_VERSION flann=FLANN_VERSION
+
+python setup.py install
---
+++ continuous_integration/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# the Travis "script" step: see http://docs.travis-ci.com/ and ../.travis.yml
+set -e
+
+cd $HOME # get out of source directory to avoid confusing nose
+
+nosetests --exe cyflann
