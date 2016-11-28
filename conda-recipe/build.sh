874d0b8fa83f69b5952bb104a6e1256ff4f36c4e
Dougal J. Sutherland <dougal@gmail.com>
2016-11-28 23:30:32 +0000

    nicer handling of dirty worktrees in conda recipe

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+$PYTHON setup.py egg_info
+sed -n -e 's/^Version: \(.*\)$/\1/p' < cyflann.egg-info/PKG-INFO > __conda_version__.txt
+
 # don't allow dirty worktrees, too easy to mess up
-if git diff-index --quiet HEAD --; then
-    echo "Dirty worktree; commit if you really want to build"
+if grep '\.dirty' __conda__version.txt; then
+    echo "No dirty worktrees; make a temp commit if you really want to build"
     git status
     exit 1
 fi
-
-$PYTHON setup.py egg_info
-sed -n -e 's/^Version: \(.*\)$/\1/p' < cyflann.egg-info/PKG-INFO > __conda_version__.txt
 
 $PYTHON setup.py install
