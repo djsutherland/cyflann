1611dee2ee355ddb7f1242128c79f7d9c60d3285
Dougal J. Sutherland <dougal@gmail.com>
2016-11-28 22:57:29 +0000

    conda-recipe: handle dirtiness better

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# don't allow dirty worktrees, too easy to mess up
+if git diff-index --quiet HEAD --; then
+    echo "Dirty worktree; commit if you really want to build"
+    git status
+    exit 1
+fi
+
 $PYTHON setup.py egg_info
 sed -n -e 's/^Version: \(.*\)$/\1/p' < cyflann.egg-info/PKG-INFO > __conda_version__.txt
 
