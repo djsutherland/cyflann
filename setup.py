d28c50acb7af25e40f51fce4a632ae578ac6bb26
Robert McLeod <robbmcleod@gmail.com>
2018-12-14 15:36:38 -0800

    Shorten conda environment check

--- setup.py
+++ setup.py
@@ -17,10 +17,8 @@ except ImportError as e:
 
 # As seen in `appveyor.yml`, if `FLANN` is installed via conda we need to 
 # set the FLANN_DIR environment variable.
-if 'CONDA_PREFIX' in os.environ:
-    CONDA_PREFIX = os.environ['CONDA_PREFIX']
-    if not 'FLANN_DIR' in os.environ:
-        os.environ['FLANN_DIR'] = os.path.join(CONDA_PREFIX, 'Library')
+if 'CONDA_PREFIX' in os.environ and not 'FLANN_DIR' in os.environ:
+    os.environ['FLANN_DIR'] = os.path.join(os.environ['CONDA_PREFIX'], 'Library')
 
 try:
     from setuptools import setup, Command
